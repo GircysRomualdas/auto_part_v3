bc595c606443a5240da8949bdba7de711905c495
Romas <r2udis@gmail.com>
2024-12-16 17:02:56 +0000

    change admin home page

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -3,5 +3,11 @@ class AdminController < ApplicationController
   layout "admin"
 
   def index
+    @orders = Order.where(fulfilled: false).order(created_at: :desc).take(5)
+    @stats = {
+      sales: Order.where(created_at: Time.now.midnight..Time.now).count,
+      revenue: Order.where(created_at: Time.now.midnight..Time.now).sum(:total_price).round(),
+      avg_sale: Order.where(created_at: Time.now.midnight..Time.now).average(:total_price).round()
+    }
   end
 end
